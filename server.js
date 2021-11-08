418391c50461cdbfbb9cfa311ffb57af3ade063f
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-11-08 12:20:35 -0600

    minor changes, still needs work

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Connected to database and listening'));
+});
